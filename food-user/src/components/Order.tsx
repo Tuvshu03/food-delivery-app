6352ebf205aaf410f22954dad9ab90fcc3614c58
Tuvshu03 <159090318+Tuvshu03@users.noreply.github.com>
2025-04-01 14:34:41 +0800

    order

--- food-user/src/components/Order.tsx
+++ food-user/src/components/Order.tsx
@@ -4,39 +4,47 @@ import { Button } from "./ui/button";
 import { axiosInstance } from "@/lib/axiosInstance";
 import axios from "axios";
 import { UserContext } from "@/utils/UserContext";
+import { ShoppingBagIcon } from "lucide-react";
 
 const MyOrder = () => {
   const [orders, setOrders] = useState([]);
-  const {userData} = useContext(UserContext)
+  const { userData } = useContext(UserContext);
   const getOrder = async () => {
     try {
-      const address = await axiosInstance.get("/product-order", {user:userData._id});
-      setOrders(address.data.ProductOrder)
+      const address = await axiosInstance.get("/product-order", {
+        user: userData._id,
+      });
+      setOrders(address.data.ProductOrder);
       console.log(address.data.ProductOrder);
     } catch (err) {
       console.log("error", err);
     }
   };
-  useEffect(()=>{
-    getOrder()
-  },[])
+  useEffect(() => {
+    getOrder();
+  }, []);
   return (
     <Card>
       <CardHeader>
         <CardTitle>Order History</CardTitle>
       </CardHeader>
-      <CardContent className="w-full h-[500px]">
-          {orders?.length>0 && orders.map((order)=>{
+      <CardContent className="w-screen h-[500px]">
+        {orders?.length > 0 &&
+          orders.map((order) => {
             return (
               <div key={order._id} className="grid w-full items-center gap-4">
-                <div className="text-foreground flex just">{order.totalPrice}{order.status}</div>
-                <div >{order.totalPrice}{}</div>
-                <div >{order.totalPrice}</div>
-                <div >{order.totalPrice}</div>
-                <div >{order.totalPrice}</div>
-
+                <div className="text-foreground w-full flex justify-between">
+                  {order.totalPrice}
+                  {order.status}
+                </div>
+                {/* {order.foodOrderItems.length>0 && order.foodOrderItems.map((food)=>{
+                  food
+                })} */}
+                {/* // <div>{order.totalPrice}</div>
+                // <div>{order.totalPrice}</div>
+                // <div>{order.totalPrice}</div> */}
               </div>
-            )
+            );
           })}
       </CardContent>
     </Card>
